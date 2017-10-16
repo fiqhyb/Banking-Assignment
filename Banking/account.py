833411aa880460762df8911051407bb755534262
Fiqhy Bismadhika <fiqhy.bismadhika@binus.ac.id>
2017-10-16 09:44:10 +0700

    Update account.py

--- Banking/account.py
+++ Banking/account.py
@@ -5,8 +5,6 @@ class Account(object):
     def getBalance(self):
         return self.balance
     def deposit(self,amt):
-        if float(amt) > 0:
-            self.balance+=float(amt)
+        self.balance+=float(amt)
     def withdraw(self,amt):
-        if float(amt) < 0:
-            self.balance-=float(amt)
+        self.balance-=float(amt)
